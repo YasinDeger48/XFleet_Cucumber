bd71df76ab80e0ed0981cccd15f52e0846f953c8
Yasin <yasin.deger48@gmail.com>
2022-06-29 17:02:49 +0300

    Framework is ready for new features

--- src/test/java/com/xfleet/runners/CukesRunner.java
+++ src/test/java/com/xfleet/runners/CukesRunner.java
@@ -7,11 +7,20 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = {
+                "pretty",
+                "html:target/reports/cucumber-report.html",
+                "json:target/reports/cucumber-report.json",
+                "rerun:target/reports/cucumber-report.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/reports/cucumber"
+        },
+        features = "src/test/resources/features", //content -> features
+        glue = "com/xfleet/step_definitions", //source -> step definitions
+        dryRun = false,
+        tags = "",
+        publish = false
 
 )
-
-
-
 
 public class CukesRunner {
 }
--- src/test/java/com/xfleet/utilities/BrowserUtils.java
+++ src/test/java/com/xfleet/utilities/BrowserUtils.java
@@ -1,4 +1,21 @@
 package com.xfleet.utilities;
 
 public class BrowserUtils {
+
+    /**
+     * Static sleep method accept second and wait during time
+     * @param seconds
+     */
+
+    public static void sleep(int seconds){
+
+        try {
+            Thread.sleep(seconds*1000);
+        } catch (InterruptedException e) {
+            System.out.println("static sleep issue");
+        }
+    }
+
+
+
 }
